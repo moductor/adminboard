cd1cedf84d933f01eff7280aba8f560785d137aa
Vojtěch Perník <develop@pervoj.cz>
2024-03-12 09:54:09 +0100

    Initialize TRPC
    https://youtu.be/qCLV0Iaq9zU

---
+++ src/app/_trpc/provider.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { httpBatchLink } from "@trpc/client";
+import { ReactNode, useState } from "react";
+import { trpc } from "./client";
+
+type Props = {
+  children?: ReactNode;
+};
+
+export default function TrpcProvider({ children }: Props) {
+  const [queryClient] = useState(() => new QueryClient({}));
+  const [trpcClient] = useState(() =>
+    trpc.createClient({
+      links: [
+        httpBatchLink({
+          url: `${getBaseUrl()}/api/trpc`,
+          async headers() {
+            return {
+              // authorization: getAuthCookie(),
+            };
+          },
+        }),
+      ],
+    }),
+  );
+
+  return (
+    <trpc.Provider client={trpcClient} queryClient={queryClient}>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </trpc.Provider>
+  );
+}
+
+function getBaseUrl() {
+  if (typeof window !== "undefined")
+    // browser should use relative path
+    return "";
+
+  // assume localhost
+  return `http://localhost:${process.env.PORT ?? 3000}`;
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import TrpcProvider from "./_trpc/provider";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -13,7 +14,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="bg-gray-50 text-gray-950 dark:bg-gray-950 dark:text-gray-50">
-        {children}
+        <TrpcProvider>{children}</TrpcProvider>
       </body>
     </html>
   );
