3eff17d31a7a246ac7777346ce8f69a44a88d1f9
Vojtěch Perník <develop@pervoj.cz>
2024-03-12 13:44:24 +0100

    Fix hydration errors with TRPC query

--- src/app/_trpc/provider.tsx
+++ src/app/_trpc/provider.tsx
@@ -10,7 +10,7 @@ type Props = {
 };
 
 export default function TrpcProvider({ children }: Props) {
-  const [queryClient] = useState(() => new QueryClient({}));
+  const queryClient = getQueryClient();
   const [trpcClient] = useState(() =>
     trpc.createClient({
       links: [
@@ -40,4 +40,31 @@ function getBaseUrl() {
 
   // assume localhost
   return `http://localhost:${process.env.PORT ?? 3000}`;
+}
+
+function makeQueryClient() {
+  return new QueryClient({
+    defaultOptions: {
+      queries: {
+        // With SSR, we usually want to set some default staleTime
+        // above 0 to avoid refetching immediately on the client
+        staleTime: 60 * 1000,
+      },
+    },
+  });
+}
+
+let browserQueryClient: QueryClient | undefined = undefined;
+function getQueryClient() {
+  if (typeof window === "undefined") {
+    // Server: always make a new query client
+    return makeQueryClient();
+  } else {
+    // Browser: make a new query client if we don't already have one
+    // This is very important so we don't re-make a new client if React
+    // suspends during the initial render. This may not be needed if we
+    // have a suspense boundary BELOW the creation of the query client
+    if (!browserQueryClient) browserQueryClient = makeQueryClient();
+    return browserQueryClient;
+  }
 }
