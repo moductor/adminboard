3afe325967c2cd2de390a4874a6c4d0df76ce28e
Vojtěch Perník <develop@pervoj.cz>
2024-03-13 18:55:23 +0100

    Rename login page component

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,7 +2,7 @@ import { isLoggedIn } from "@/utils/sessionServer";
 import { redirect } from "next/navigation";
 import LoginForm from "./LoginForm";
 
-export default async function Home() {
+export default async function Page() {
   if (isLoggedIn()) redirect("/");
 
   return (
