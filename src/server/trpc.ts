d3be5f15de6acce8c0d604607fb4d62ca6276481
Vojtěch Perník <develop@pervoj.cz>
2024-03-13 18:52:43 +0100

    Add user session to TRPC context

--- src/server/trpc.ts
+++ src/server/trpc.ts
@@ -1,15 +1,18 @@
+import { getCurrentUser } from "@/utils/sessionServer";
 import { initTRPC } from "@trpc/server";
 import { FetchCreateContextFnOptions } from "@trpc/server/adapters/fetch";
 import { NextRequest } from "next/server";
-
-const t = initTRPC.context<typeof createContext>().create();
-
-export const router = t.router;
-export const publicProcedure = t.procedure;
 
 export function createContext({
   req,
   resHeaders,
 }: FetchCreateContextFnOptions) {
-  return { req: req as NextRequest, resHeaders };
+  return { req: req as NextRequest, resHeaders, userId: getCurrentUser() };
 }
+
+export type TRPCContext = typeof createContext;
+
+const t = initTRPC.context<TRPCContext>().create();
+
+export const router = t.router;
+export const publicProcedure = t.procedure;
