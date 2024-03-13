e7109e51cf614e992843e66b14e22e4eb365e245
Vojtěch Perník <develop@pervoj.cz>
2024-03-13 18:54:20 +0100

    Redirect from login screen when loggged in

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,10 @@
+import { isLoggedIn } from "@/utils/sessionServer";
+import { redirect } from "next/navigation";
 import LoginForm from "./LoginForm";
 
 export default async function Home() {
+  if (isLoggedIn()) redirect("/");
+
   return (
     <div className="grid min-h-screen place-content-center p-4">
       <LoginForm />
