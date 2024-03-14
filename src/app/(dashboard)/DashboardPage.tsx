2f36b8b63072e2fe9698a0ccf0f42eef9af08729
Vojtěch Perník <develop@pervoj.cz>
2024-03-14 14:08:00 +0100

    Add suffix to dashboard page title

--- src/app/(dashboard)/DashboardPage.tsx
+++ src/app/(dashboard)/DashboardPage.tsx
@@ -3,12 +3,16 @@ import { ReactNode } from "react";
 type Props = {
   title: string;
   children?: ReactNode;
+  suffix?: ReactNode;
 };
 
-export default function DashboardPage({ title, children }: Props) {
+export default function DashboardPage({ title, children, suffix }: Props) {
   return (
     <main className="grid gap-8">
-      <h1 className="text-4xl font-bold">{title}</h1>
+      <div className="flex flex-wrap items-center justify-between gap-4">
+        <h1 className="text-4xl font-bold">{title}</h1>
+        {suffix && <div>{suffix}</div>}
+      </div>
       <div>{children}</div>
     </main>
   );
