f48226047a928928fce3f5f0c0312361eff0e7a6
Vojtěch Perník <develop@pervoj.cz>
2024-03-14 14:04:46 +0100

    Fix button styling

--- src/components/FormButton.tsx
+++ src/components/FormButton.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 import { tv } from "tailwind-variants";
 
 export const formButtonClass = tv({
-  base: "rounded-lg bg-gray-900 px-3 py-2 font-bold leading-none text-gray-50 focus:outline-none focus:ring dark:bg-gray-50 dark:text-gray-950",
+  base: "inline-block rounded-lg bg-gray-900 px-3 py-2 text-center font-bold leading-none text-gray-50 focus:outline-none focus:ring dark:bg-gray-50 dark:text-gray-950",
 });
 
 type Props = JSX.IntrinsicElements["button"] & { children?: ReactNode };
