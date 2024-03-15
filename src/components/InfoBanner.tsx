e5d32109b52dfad2f134b59fea8ee9e52aa3fd60
Vojtěch Perník <develop@pervoj.cz>
2024-03-15 13:00:33 +0100

    Add way to add icons to InfoBanner

--- src/components/InfoBanner.tsx
+++ src/components/InfoBanner.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 import { tv } from "tailwind-variants";
 
 export const infoBannerClass = tv({
-  base: "text-wrap rounded-lg border border-opacity-20 bg-opacity-10 p-2",
+  base: "grid gap-1 text-wrap rounded-xl border border-opacity-20 bg-opacity-10 p-2",
   variants: {
     color: {
       info: "border-sky-500 bg-sky-500 text-sky-500",
@@ -15,16 +15,30 @@ export const infoBannerClass = tv({
 type Props = JSX.IntrinsicElements["p"] & {
   children?: ReactNode;
   variant?: typeof infoBannerClass.defaultVariants;
+  title?: string;
+  icon?: ReactNode;
 };
 
 export default function InfoBanner({
   className,
   children,
   variant,
+  title,
+  icon,
   ...props
 }: Props) {
   return (
     <div className={infoBannerClass({ className, ...variant })} {...props}>
+      {title && (
+        <p
+          className="grid grid-cols-[1em_1fr] items-center gap-1 text-lg font-bold"
+          role="heading"
+        >
+          {icon && <span>{icon}</span>}
+          <span className="">{title}</span>
+        </p>
+      )}
+
       {children}
     </div>
   );
