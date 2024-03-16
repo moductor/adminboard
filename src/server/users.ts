f661ed9d52c33f2ee4d64b7b2ddcd01e0cea53e3
Vojtěch Perník <develop@pervoj.cz>
2024-03-16 12:01:14 +0100

    Authorize user creating API

--- src/server/users.ts
+++ src/server/users.ts
@@ -3,12 +3,15 @@ import { createUser } from "@/wildduck/users";
 import { CreateUserBodyParameterModel } from "@absolit/simple-wildduck";
 import { TRPCError } from "@trpc/server";
 import typia from "typia";
+import { authorize } from "./auth";
 import { publicProcedure, router } from "./trpc";
 
 export const usersRouter = router({
   create: publicProcedure
     .input(typia.createAssertEquals<CreateUserBodyParameterModel>())
-    .mutation(async ({ input }) => {
+    .mutation(async ({ input, ctx }) => {
+      authorize(ctx, { permissions: ["users.create"] });
+
       const res = await createUser(input);
 
       if (!res.error) return res.data.userId;
