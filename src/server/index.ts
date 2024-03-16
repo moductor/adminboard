4618b3da7d268b692efa8c6a54a76aab9bd9de36
Vojtěch Perník <develop@pervoj.cz>
2024-03-16 12:02:54 +0100

    Add account router

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,3 +1,4 @@
+import { accountRouter } from "./account";
 import { authRouter } from "./auth";
 import { router } from "./trpc";
 import { usersRouter } from "./users";
@@ -5,6 +6,7 @@ import { usersRouter } from "./users";
 export const appRouter = router({
   auth: authRouter,
   users: usersRouter,
+  account: accountRouter,
 });
 
 export type AppRouter = typeof appRouter;
